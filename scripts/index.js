72ae4dd19035647299f5324b3cb3de0fa16820ca
Mary Nikolaychuk <ktotutmeow@yandex.ru>
2023-12-02 22:19:10 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,42 @@
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки 
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+
+
+function addCard (card, deleteCard) {
+        
+    const cardElement = cardTemplate.cloneNode(true);
+            
+    cardElement.querySelector('.card__description').textContent = card.name;    
+    cardElement.querySelector('.card__image').src = card.link;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');         
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement
+
+}
+
+function deleteCard (event) {
+    const deletingCard = event.target.closest('.card'); 
+    deletingCard.remove();
+}
+
+
+function showCard (cardElement) {
+    cardList.append(cardElement);
+}
+
+  
+initialCards.forEach(function (card) {
+    const initialCard = addCard(card, deleteCard);
+    showCard(initialCard);
+});
